6e0512e6b0b84ac594b0c466c8a6d9b755a6bd3b
antosei <anthonyosei@vt.edu>
2025-11-18 16:13:59 -0500

    implement MainTabView with tab items for navigation

---
+++ Stylist/MainTabView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+// Main file for tab view
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+
+            Closet()
+                .tabItem {
+                    Label("Closet", systemImage: "hanger")
+                }
+
+            OutfitRecommendation()
+                .tabItem {
+                    Label("Outfits", systemImage: "shirt.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+        .environment(ModelData())
+        .environmentObject(AuthViewModel())
+}
