80e1176333b8c3b57b4e358ed24bc0f4449f033f
antosei <anthonyosei@vt.edu>
2025-11-24 22:18:53 -0500

    Initialize modelData and authVM in StylistApp

--- Stylist/StylistApp.swift
+++ Stylist/StylistApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct StylistApp: App {
+    @State private var modelData = ModelData()
+    @StateObject private var authVM = AuthViewModel()
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)          // shared closet and currentUser
+                .environmentObject(authVM)       // shared auth session
         }
+        .modelContainer(for: User.self)
     }
 }
